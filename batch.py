18bebf45fb48a9888be56c6f37d6a4c24b331c33
dhakim87 <dhakimdahakim@gmail.com>
2021-09-08 13:23:03 -0700

    Made batch.py parallelizable

--- batch.py
+++ batch.py
@@ -10,13 +10,25 @@ print("In Folder: " + sys.argv[1])
 print("Out Folder: " + sys.argv[2])
 print("Executable Path: " + sys.argv[3])
 
+job_id = 0
+num_jobs = 1
+
+if len(sys.argv) == 6:
+    job_id = int(sys.argv[4])
+    num_jobs = int(sys.argv[5])
+
 sams = glob.glob(sys.argv[1] + "/*.sam")
 xzsams = glob.glob(sys.argv[1] + "/*.sam.xz")
 
 all_sams = sams + xzsams
 
-for f in all_sams:
+i = -1
+for f in sorted(all_sams):
+    i += 1
+    if i % num_jobs != job_id:
+        continue
+
     if f.endswith(".sam"):
-        subprocess.run("echo cat " + f + " | " + sys.argv[3] + " > " + sys.argv[2] + "/" + os.path.basename(f)[:-4] + ".outsam", shell=True)
+        subprocess.run("cat " + f + " | " + sys.argv[3] + " > " + sys.argv[2] + "/" + os.path.basename(f)[:-4] + ".outsam", shell=True)
     elif f.endswith(".sam.xz"):
-        subprocess.run("echo xzcat " + f + " | " + sys.argv[3] + " > " + sys.argv[2] + "/" + os.path.basename(f)[:-7] + ".outsam", shell=True)
+        subprocess.run("xzcat " + f + " | " + sys.argv[3] + " > " + sys.argv[2] + "/" + os.path.basename(f)[:-7] + ".outsam", shell=True)
